0cde2212fd21b7723aea80b239df0fbf5edd4b9a
Julie Spackman <spack84@byui.edu>
2023-09-21 23:08:04 -0600

    moving Wk2 Team Project back into its proper foldr

--- Wk2TeamProject/backend/routes/professional.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const routes = express.Router();
-const profController = require('../controllers/professional');
- 
-routes.get('/', profController.getData);
-// this'll happen at localhost:8080/professional/
-// routes.get('/second', profController.secondRoute);
-
-module.exports = routes;
