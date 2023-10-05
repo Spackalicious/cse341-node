eb77267754b2bfe0f4bb7d7890b1dbe336723d96
Julie Spackman <spack84@byui.edu>
2023-10-04 22:51:43 -0600

    Wk4 3rd update :)

--- swagger.js
+++ swagger.js
@@ -16,8 +16,8 @@ const endpointsFiles = ['./routes/index.js'];
    'endpointsFiles' only the root file where the route starts,
    such as index.js, app.js, routes.js, ... */
 
-// swaggerAutogen(outputFile, endpointsFiles, doc);
+swaggerAutogen(outputFile, endpointsFiles, doc);
 
-swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
-    require('./server.js'); // Your project's root file
-  });
+// swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
+//     require('./server.js'); // Your project's root file
+//   });
