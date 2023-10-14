c73a1d5862c74236dac3f9a9aea9ed82e90aeba6
Julie Spackman <spack84@byui.edu>
2023-10-13 22:05:23 -0600

    Wk05 updating render URL

--- 0.PersonalProject_MusicStudents/swagger.js
+++ 0.PersonalProject_MusicStudents/swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'CSE341 Personal Project - Music Students & Books',
   },
   // host: 'localhost:8080',
-  host: 'https://js-musicstudents.onrender.com',
+  host: 'https://js-musicstudents-cse341.onrender.com',
   schemes: ['https'],
   // schemes: ['http']
 };
